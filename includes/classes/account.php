a9de0b1ca34b7a297692c9814a360b8c6f7e72e6
ariana124 <ariana.bibiano@gmail.com>
2020-04-05 00:03:27 +0000

    validates account creation

---
+++ includes/classes/account.php
@@ -0,0 +1,97 @@
+<?php
+
+    class Account {
+
+        private $errorArray;
+
+        public function __construct() {
+            $this->errorArray = array();
+            // This sets the $errorArray variable to an empty array
+        }
+
+        public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
+            $this->validateUsername($un);
+            $this->validateFirstName($fn);
+            $this->validateLastName($ln);
+            $this->validateEmails($em, $em2);
+            $this->validatePasswords($pw, $pw2);
+
+            if (empty($this->errorArray == true)) {
+                // Insert into database
+                return true;
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        public function getError($error) {
+            if (!in_array($error, $this->errorArray)) {
+                // Checks to see if the error passed in, is in the array
+                $error = "";
+            }
+            return "<span class='errorMessage'>$error</span>";
+        }
+
+        // The following functions can only be called from inside this class
+        private function validateUsername($un) {
+            if (strlen($un) > 25 || strlen($un) < 5) {
+                array_push($this->errorArray, "Your username must be between 5 and 25 characters");
+                return;
+            }
+
+            // If statement to check if username exists
+        }
+        
+        private function validateFirstName($fn) {
+            if (strlen($fn) > 25 || strlen($fn) < 1) {
+                array_push($this->errorArray, "Your first name must be between 1 and 25 characters");
+                return;
+            }
+        }
+        
+        private function validateLastName($ln) {
+            if (strlen($ln) > 25 || strlen($ln) < 2) {
+                array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
+                return;
+            }
+        }
+        
+        private function validateEmails($em, $em2) {
+            if ($em != $em2) {
+                array_push($this->errorArray, "Your emails do not match");
+                return;
+            }
+
+            if (!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+                // Checks to make sure the email is valid, with endings such as .com, .edu, etc.
+                array_push($this->errorArray, "Email is invalid");
+                return;
+            }
+
+            // If statement to check that username hasn't been used
+
+        }
+        
+        private function validatePasswords($pw, $pw2) {
+            if ($pw != $pw2) {
+                array_push($this->errorArray, "Your passwords do not match");
+                return;
+            }
+
+            if (preg_match('/[^A-za-z0-9]/', $pw)) {
+                array_push($this->errorArray, "Your password can only contain letters and numbers");
+                return;
+            }
+
+            if (strlen($pw) > 28 || strlen($pw) < 7) {
+                array_push($this->errorArray, "Your password must be between 7 and 28 characters");
+                return;
+            }
+        }
+        
+
+    }
+
+?>
