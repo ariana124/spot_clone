28582c36831142e9addfff57527aac0401a444f6
ariana124 <ariana.bibiano@gmail.com>
2020-04-05 20:00:20 +0000

    created public static variable for error msgs

---
+++ includes/classes/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+    class Constants {
+
+        public static $passwordsDoNotMatch = "Your passwords do not match";
+        public static $passwordsNotAlphanumeric = "Your password can only contain letters and numbers";
+        public static $passwordsLength = "Your password must be between 7 and 28 characters";
+        public static $emailInvalid = "Email is invalid";
+        public static $emailsDoNotMatch = "Your emails do not match";
+        public static $firstNameLength = "Your first name must be between 1 and 25 characters";
+        public static $lastNameLength = "Your last name must be between 2 and 25 characters";
+        public static $usernameLength = "Your username must be between 5 and 25 characters";
+    }
+?>
