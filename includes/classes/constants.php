86649b0b5c55e641c9e4497208c9267b06aed694
ariana124 <ariana.bibiano@gmail.com>
2020-04-15 00:25:21 +0000

    added email/username/login failed or taken constants

--- includes/classes/constants.php
+++ includes/classes/constants.php
@@ -5,10 +5,17 @@
         public static $passwordsDoNotMatch = "Your passwords do not match";
         public static $passwordsNotAlphanumeric = "Your password can only contain letters and numbers";
         public static $passwordsLength = "Your password must be between 7 and 28 characters";
+
         public static $emailInvalid = "Email is invalid";
         public static $emailsDoNotMatch = "Your emails do not match";
+	public static $emailTaken = "This email is already in use";
+
         public static $firstNameLength = "Your first name must be between 1 and 25 characters";
         public static $lastNameLength = "Your last name must be between 2 and 25 characters";
+
         public static $usernameLength = "Your username must be between 5 and 25 characters";
+	public static $usernameTaken = "This username already exists";
+
+	public static $loginFailed = "Your username or password was incorrect";
     }
 ?>
