fc0a52cfb3cc5ca7738311da9ec6680585f3cbed
ariana124 <ariana.bibiano@gmail.com>
2020-04-15 00:18:05 +0000

    added config file for use of session

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+//allows index page to use session without creating another session here
+include("includes/config.php");
+
+//session_destroy(); only include this when you want to logout manually (refreshing the index page)
+
+if (isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+    header("Location: register.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
