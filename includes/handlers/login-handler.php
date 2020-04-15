56e828319b8fa253f58b5476f87bf7d44a4c697f
ariana124 <ariana.bibiano@gmail.com>
2020-04-15 00:30:13 +0000

    added if to keep user logged in

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -2,6 +2,16 @@
 
 if (isset($_POST['loginButton'])) {
         // Login button was pressed
+        $username = $_POST['loginUsername'];
+        $password = $_POST['loginPassword'];
+
+        $result = $account->login($username, $password);
+
+        if ($result == true) {
+            // userLoggedIn wil be set to the user that logged in
+            $_SESSION['userLoggedIn'] = $username;
+            header("Location: index.php");
+        }
     }
 
 ?>
