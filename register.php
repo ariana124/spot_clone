6242e9cffa52d8b2a2e94d98ea1520cdf26aa820
ariana124 <ariana.bibiano@gmail.com>
2020-04-18 00:58:04 +0000

    added jquery and ref to javascript function

--- register.php
+++ register.php
@@ -21,6 +21,8 @@
     <head>
         <title>Welcome to SpotClone</title>
         <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="assets/js/register.js"></script>
     </head>
     <body>
         <div id="background">
