c12f8d6a0a7018aa5926b31f4e6a991121dd4ef9
ariana124 <ariana.bibiano@gmail.com>
2020-04-21 04:49:48 +0000

    created div for volumeBar

--- index.php
+++ index.php
@@ -94,7 +94,21 @@ else {
                     </div>
                 </div>
 
-                <div id="nowPlayingRight"></div>
+                <div id="nowPlayingRight">
+                    <div class="volumeBar">
+
+                        <button class="controlButton volume" title="Volume button">
+                            <img src="assets/images/icons/volume.png" alt="Volume">
+                        </button>
+
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+
+                    </div>
+		</div>
 
             </div>
 
