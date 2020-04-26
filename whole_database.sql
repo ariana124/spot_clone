cf95622f8ead91d23400dc007bd4192b8f971c47
ariana124 <ariana.bibiano@gmail.com>
2020-04-26 03:57:32 +0000

    added user table structure

--- whole_database.sql
+++ whole_database.sql
@@ -5,6 +5,19 @@ CREATE DATABASE IF NOT EXISTS spot_clone;
 
 USE spot_clone;
 
+-- Table structure for the table 'users'
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT AUTO INCREMEMT NOT NULL,
+       username VARCHAR(25) NOT NULL,
+       firstName VARCHAR(50) NOT NULL,
+       lastName VARCHAR(50) NOT NULL,
+       email VARCHAR(50) NOT NULL,
+       password VARCHAR(32) NOT NULL,
+       signUpDate DATETIME NOT NULL,
+       profilePic VARCHAR(500) NOT NULL,
+       PRIMARY KEY (id));
+
 -- Table structure for the table 'artists'
 
 CREATE TABLE IF NOT EXISTS artists (
