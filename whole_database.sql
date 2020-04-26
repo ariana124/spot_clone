1e93f4d5d02d16637c7b9998745bc9e2c74c574a
ariana124 <ariana.bibiano@gmail.com>
2020-04-26 03:44:46 +0000

    database creation

---
+++ whole_database.sql
@@ -0,0 +1,42 @@
+-- Creates the database for the spotify clone with the following tables:
+--    artists, albums, songs, genres, users
+
+CREATE DATABASE IF NOT EXISTS spot_clone;
+
+USE spot_clone;
+
+-- Table structure for the table 'artists'
+
+CREATE TABLE IF NOT EXISTS artists (
+       id INT AUTO INCREMENT NOT NULL,
+       name VARCHAR(50) NOT NULL,
+       PRIMARY KEY (id));
+
+-- Table structure for the table 'genres'
+
+CREATE TABLE IF NOT EXISTS genres (
+       id INT AUTO INCREMENT NOT NULL,
+       name VARCHAR(50) NOT NULL,
+       PRIMARY KEY (id));
+
+-- Table structure for the table 'albums'
+
+CREATE TABLE IF NOT EXISTS albums (
+       id INT AUTO INCREMENT NOT NULL,
+       title VARCHAR(100) NOT NULL,
+       artist INT NOT NULL,
+       genre INT NOT NULL,
+       artworkPath VARCHAR(500)
+       PRIMARY KEY (id));
+
+-- Table structure for the table 'songs'
+
+CREATE TABLE IF NOT EXISTS songs (
+       id INT AUTO INCREMENT NOT NULL,
+       title VARCHAR(200) NOT NULL,
+       artist INT NOT NULL,
+       duration VARCHAR(8) NOT NULL,
+       path VARCHAR(500) NOT NULL,
+       albumOrder INT NOT NULL,
+       plays INT NOT NULL
+       PRIMARY KEY (id));
