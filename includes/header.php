310bd93039fd7d408075ecdfeffc6cf1a656d33e
ariana124 <ariana.bibiano@gmail.com>
2020-04-27 03:42:41 +0000

    new header file for header portion of the original index.php file

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+?php
+//allows index page to use session without creating another session here
+include("includes/config.php");
+
+//session_destroy(); only include this when you want to logout manually (refreshing the index page)
+
+if (isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+    header("Location: register.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SpotClone</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+    </head>
+    <body>
+
+        <div id="mainContainer">
+
+            <div id="topContainer">
+
+                <!-- at runtime pastes the code from the navBarContainer.php file here into the index.php -->
+                <?php include("includes/navBarContainer.php"); ?>
+
+                <div id="mainViewContainer">
+
+                    <div id="mainContent">
