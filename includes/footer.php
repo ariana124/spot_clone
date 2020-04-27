1561b0068d2b1cf4ec01da4afe818b5f578358f3
ariana124 <ariana.bibiano@gmail.com>
2020-04-27 03:45:25 +0000

    new footer file for bottom portion of the original index.php file

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+	<!-- footer portion of the original index.php file -->
+                    </div>
+
+                </div>
+
+            </div>
+
+            <?php include("includes/nowPlayingBar.php"); ?>
+
+        </div>
+
+    </body>
+</html>
