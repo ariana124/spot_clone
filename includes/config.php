f8d1909ce682229045d12ca775cf79b5ab2d4d36
ariana124 <ariana.bibiano@gmail.com>
2020-05-06 04:26:47 +0000

    edited comments

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,17 @@
 <?php
-    // Turns on output buffering
-    // When php pays loads it sends data to the servers in pieces
+    // Turns on output buffering.
+    // When php pays loads it sends data to the servers in pieces.
     ob_start();
     session_start();
 
     $timezone = date_default_timezone_set("America/Los_Angeles");
 
     // Takes the parameters: servername, username, password, & database
-    // The variable $con is the connection to the database
+    // The variable $con is the connection to the database.
     $con = mysqli_connect("localhost", "root", "", "spot_clone");
 
     if (mysqli_connect_errno()) {
-        // The dot appends the error message
+        // The dot appends the error message (the dot is used to append strings).
         echo "Failed to connect: " . mysqli_connect_errno();
     }
 ?>
